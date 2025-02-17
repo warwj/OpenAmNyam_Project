6ab80562b901590617ece7c6dbd3b67d475ab3ee
KirpichevIvan <kirpichev2002@mail.ru>
2025-02-18 00:32:50 +0300

    исправлен port

--- app.py
+++ app.py
@@ -178,4 +178,4 @@ def download_file(filename):
     return send_file(file_path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, threaded=True, host="0.0.0.0", port=80)
